6dab23b292c051cafeada58dd777229508d4ea42
Anastasya Ivanova <nastya1615325@gmail.com>
2022-07-11 09:32:29 +0400

    Домашнее задание выполнено

---
+++ ObjectsAndClasses_Encapsulation/src/Dimensions.java
@@ -0,0 +1,17 @@
+public class Dimensions {
+    private final double width;
+    private final double height;
+    private final double length;
+
+    public Dimensions( double width, double height, double length){
+        this.width = width;
+        this.height = height;
+        this.length = length;
+    }
+
+    public double volume () {
+        return width * height * length;
+    }
+
+
+}
